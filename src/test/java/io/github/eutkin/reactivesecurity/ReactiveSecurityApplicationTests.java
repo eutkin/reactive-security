b6e4279672c728d69bf982d40014efaf88622206
eutkin <eutkin@uma.tech>
2020-02-13 18:15:12 +0300

    Initial commit

---
+++ src/test/java/io/github/eutkin/reactivesecurity/ReactiveSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.eutkin.reactivesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
